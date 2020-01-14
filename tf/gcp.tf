a058741212e47618652237a90adc9a6d3dc306ca
Denys Stroebel <denys.stroebel@dimensiondata.com>
2020-01-14 11:44:08 +0200

    Fix GKE node and cluster versions; Add node oauth scopes

--- tf/gcp.tf
+++ tf/gcp.tf
@@ -6,7 +6,7 @@ provider "google" {}
 resource "google_container_cluster" "primary" {
   name               = "go-arithmetic-cluster"
   location           = "europe-west1"
-  min_master_version = "1.14"
+  min_master_version = "1.14.9-gke.2"
 
   remove_default_node_pool = true
   initial_node_count       = 1
@@ -18,7 +18,7 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
   location   = "europe-west1"
   cluster    = "${google_container_cluster.primary.name}"
   node_count = 1
-  version    = "1.14"
+  version    = "1.14.9-gke.2"
 
   node_config {
     preemptible  = true
@@ -31,6 +31,11 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
     oauth_scopes = [
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/pubsub",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/trace.append",
     ]
   }
 }
